3f1dd4f9ae48c1df3d422e0518cd0182d3ab299b
ericchowder <linksta7@gmail.com>
2022-01-20 17:53:25 -0500

    Set up user model and schema

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,41 @@
 import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+# Init app
 app = Flask(__name__)
+# Current directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database in correct directory
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "db.sqlite")
+# Suppress complaints
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+
+# Product Class/Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+    # Constructor
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'public_id', 'name', 'password', 'admin')
+
+# Init Schema
+user_schema = UserSchema(strict=True)
 
 @app.route('/')
 def hello_world():
@@ -11,6 +45,7 @@ def hello_world():
 def get():
     return jsonify({ 'msg': 'returning Hello World' })
 
+# Server
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
